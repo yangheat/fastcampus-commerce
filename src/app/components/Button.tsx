d9fd559a009ab981a4202b906b9f8900f75910fa
yangheat <yangheat@gamil.com>
2024-05-21 13:19:32 +0900

    feat: utilize emotion library

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,3 +1,15 @@
-export default function Button() {
-  return <div>Button</div>
-}
+import styled from '@emotion/styled'
+
+const Button = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
+
+export default Button
--- src/app/components/InputButton.tsx
+++ src/app/components/InputButton.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useRef } from 'react'
+import { css } from '@emotion/react'
+import Button from './Button'
 
 export default function InputButton() {
   const inputRef = useRef<HTMLInputElement>(null)
@@ -18,8 +20,24 @@ export default function InputButton() {
   }
   return (
     <>
-      <input ref={inputRef} type="text" placeholder="name" />
-      <button onClick={handleClick}>Add Jacket</button>
+      <input
+        ref={inputRef}
+        type="text"
+        className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+        placeholder="name"
+      />
+      <button
+        onClick={handleClick}
+        className="flex justify-center"
+        css={css`
+          background-color: pink;
+          padding: 16px;
+          border-radius: 8px;
+        `}
+      >
+        Add Jacket
+      </button>
+      <Button onClick={handleClick}>Add Jacket</Button>
     </>
   )
 }
