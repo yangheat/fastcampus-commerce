7b1a1786e0a35f0a1b71dd906e4bf9c478ad8bae
yangheat <yangheat@gamil.com>
2024-07-23 00:32:09 +0900

    feat: 카테고리를 통한 검색과 로딩 기능 추가
    from 함수를 지역 변수로 이동
    - eq() 함수가 전역으로 설정되는 경우 eq는 중복 적용이 됨.

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -8,27 +8,32 @@ const supabaseUrl = 'https://gpztwglyopczhoutiqgh.supabase.co'
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwenR3Z2x5b3BjemhvdXRpcWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0OTc1MDAsImV4cCI6MjAzNTA3MzUwMH0.URlsIEdjSB5WCJhtApkBkIlkW7hBVt3yzdswTNLilPw'
 const supabase = createClient(supabaseUrl, supabaseKey)
-const PRODUCTS = supabase.from('products')
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const id = searchParams.get('id')
   const start = searchParams.get('start')
   const end = searchParams.get('end')
+  const category = searchParams.get('category')
 
-  let select
+  const PRODUCTS = supabase.from('products')
+  let query
   if (start && end) {
     // 페이징 계산
-    select = PRODUCTS.select('*', { count: 'exact' }).range(
+    query = PRODUCTS.select('*', { count: 'exact' }).range(
       Number(start),
       Number(end)
     )
   } else {
     // id 검색
-    select = PRODUCTS.select().eq('id', id).single()
+    query = PRODUCTS.select().eq('id', id).single()
   }
 
-  const { data, count, error } = await select
+  if (category) {
+    query = PRODUCTS.select().eq('category_id', Number(category))
+  }
+
+  const { data, count, error } = await query
 
   if (error) {
     console.error('error', error)
@@ -50,6 +55,7 @@ export async function POST(request: NextRequest) {
   const body = await request.json()
   const id = body.id
   const contents = body.contents
+  const PRODUCTS = supabase.from('products')
 
   if (!id) {
     return NextResponse.json({ error: 'No id' }, BAD_REQUEST)
--- src/app/products/page/page.tsx
+++ src/app/products/page/page.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import { Pagination } from '@mantine/core'
+import { Pagination, SegmentedControl } from '@mantine/core'
 import { CATEGORY_MAP, TAKE } from '@/app/constants/products'
 
 type PRODUCT = {
@@ -12,9 +12,12 @@ type PRODUCT = {
 }
 
 export default function Page() {
+  const [category, setCategory] = useState('All')
   const [products, setProducts] = useState<PRODUCT[]>([])
   const [activePage, setPage] = useState(1)
   const [totalPage, setTotalPage] = useState(0)
+  const [loading, setLoading] = useState(true)
+
   /** 페이징 계산식
    * start: (n-1) * 9
    * end: n * 8 + (n-1)
@@ -22,38 +25,64 @@ export default function Page() {
   useEffect(() => {
     const start = (activePage - 1) * TAKE
     const end = activePage * (TAKE - 1) + (activePage - 1)
+    const categoryIndex = CATEGORY_MAP.indexOf(category)
+
+    let url = `/api/products?start=${start}&end=${end}`
+
+    setLoading(true)
+    if (categoryIndex !== -1) {
+      url += `&category=${categoryIndex + 1}`
+    }
 
     axios
-      .get(`/api/products?start=${start}&end=${end}`)
+      .get(url)
       .then((result) => {
         const data = result.data
         setProducts(data.product)
         setTotalPage(Math.ceil(data.total / TAKE))
+        setLoading(false)
       })
       .catch((error) => console.error(error))
-  }, [activePage])
+  }, [activePage, category])
 
   return (
     <main className="my-32">
+      <section className="flex mb-5">
+        <SegmentedControl
+          value={category}
+          onChange={(curr) => {
+            setPage(1)
+            setCategory(curr)
+          }}
+          data={['All', ...CATEGORY_MAP]}
+          className="m-auto"
+        />
+      </section>
       <article className="grid grid-cols-3 px-32  gap-5">
-        {products.map(({ id, price, category_id }: PRODUCT) => (
-          <div key={id} className="justify-self-center">
-            <Image
-              className="rounded"
-              src={`https://picsum.photos/id/${id}/300/200`}
-              alt="image"
-              width={300}
-              height={200}
-              placeholder="blur"
-              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
-            />
-            <div className="flex justify-between">
-              <span>{id}</span>
-              <span>{price}원</span>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          products.map(({ id, price, category_id }: PRODUCT) => (
+            <div key={id} className="justify-self-center">
+              <Image
+                className="rounded"
+                src={`https://picsum.photos/id/${id}/300/200`}
+                alt="image"
+                width={300}
+                height={200}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
+              />
+              <div className="flex justify-between">
+                <span>{id}</span>
+                <span>{price}원</span>
+              </div>
+              <span className="text-zinc-400">
+                {CATEGORY_MAP[category_id - 1]}
+              </span>
             </div>
-            <span className="text-zinc-400">{CATEGORY_MAP[category_id]}</span>
-          </div>
-        ))}
+          ))
+        )}
       </article>
       <section className="flex mt-5">
         <Pagination
